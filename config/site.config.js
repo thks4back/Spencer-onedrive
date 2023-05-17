ee655ff8ee5338388ed34ba900eb25d8f58977ed
thks4back <116699190+thks4back@users.noreply.github.com>
2023-05-17 09:35:42 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: [
-    'https://thks4back-onedrive.vercel.app/hihi',
+    '/hihi',
   ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
